c5c1e18a22bedd58e002b86f4bf5f27778cd3b7d
Patrick Dohn <dohnpatrick1993@gmail.com>
2022-03-22 13:43:03 -0400

    Updates

---
+++ precache-manifest.286ce45da4e1fbedea0510d1bd10b660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addfcd22683877f3cfa3b23fddc2866a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5821225def503443f24",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8cf9e49f2e2717c02dd9",
+    "url": "./static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "a5821225def503443f24",
+    "url": "./static/js/2.9f6194ec.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.9f6194ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf9e49f2e2717c02dd9",
+    "url": "./static/js/main.8f804b66.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
